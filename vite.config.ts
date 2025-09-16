3635654b31af20c5e2f5547b0c2f93ee41b55474
JuanMiguel-Good <juan.miguel@goodsolutions.pe>
2025-09-16 18:38:34 -0500

    Subir proyecto completo para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/amigosecreto/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  }
 });
